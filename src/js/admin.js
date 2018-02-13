7aec86919826433f3f0948eb4e87ebc1f66e7c48
Rishabh Kumar Jha <rishabhkumarjha@gmail.com>
2018-02-13 21:42:25 +0000

    admin module added to view active peers and block information

---
+++ src/js/admin.js
@@ -0,0 +1,43 @@
+Admin = {
+    web3Provider: null,
+    web3: null,
+    coinbase:null,
+    coinbase_balance:null,
+    init:function(){
+        //Admin.web3Provider = web3.currentProvider;
+        Admin.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+        Admin.web3 = new Web3(Admin.web3Provider);
+        //var admin_console = $('disp');
+        console.log(Admin.web3);
+        Admin.coinbase = Admin.web3.eth.coinbase
+        Admin.coinbase_balance = Admin.web3.eth.getBalance(Admin.coinbase);
+        document.getElementById('coinbase').innerText = Admin.web3.eth.coinbase;
+        document.getElementById('coinbase_balance').innerText = Admin.coinbase_balance;
+        document.getElementById('peers').innerText = Admin.web3.net.peerCount;
+        
+    }
+
+}
+
+function getBlock(){
+    //alert("hi")
+    var block_no = document.getElementById('block_no').value;
+    var block_container = document.getElementById('block').innerHTML;
+    var block = Admin.web3.eth.getBlock(block_no);
+    document.getElementById('block').innerHTML = "Number : " + block.number +'<br>';
+    document.getElementById('block').innerHTML += "Difficulty : " + block.difficulty +'<br>';
+    document.getElementById('block').innerHTML += "Extra Data : " + block.extraData +'<br>';
+    document.getElementById('block').innerHTML += "Hash : " + block.hash +'<br>';
+    document.getElementById('block').innerHTML += "Parent Hash : " + block.parentHash +'<br>';
+    document.getElementById('block').innerHTML += "Miner : " + block.miner +'<br>';
+    document.getElementById('block').innerHTML += "Transactions : " + block.transactions +'<br>';
+    document.getElementById('block').innerHTML += "Timestamp : " + block.timestamp +'<br>';
+    document.getElementById('block').innerHTML += "Size : " + block.size +'<br>';
+    console.log(Admin.web3.eth.getBlock(block_no)); 
+}
+
+$(function() {
+    $(window).load(function() {
+      Admin.init();
+    });
+  });
