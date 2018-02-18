bcea05c504fa8a290f70ebafe0a4b272ee53d160
rishabh <rismabhkumarjha@gmail.com>
2018-02-18 23:11:38 +0900

    dynamic webpage for transactiontions created as tranasaction.html

--- src/js/app1.js
+++ src/js/app1.js
@@ -62,7 +62,7 @@ function onButtonAdd()
   //console.log(account);
   
 
-App.handle.addProperty(document.getElementById("owner_addr").value,
+App.handle.addProperty(document.getElementById("owner_address").value,
                                   document.getElementById("property_id").value,{from:account})
                                   .then(function(result){
                                       return App.handle.getAddress.call(document.getElementById("property_id").value)})
@@ -85,7 +85,7 @@ function onButtonBuy()
         console.log(result);});*/
     var account;
     var owner=null;
-    var prop_id = document.getElementById("prop_id").value;
+    var property_id = document.getElementById("property_id").value;
     web3.eth.getAccounts(function(error, accounts) 
     {
       if (error) 
@@ -95,16 +95,16 @@ function onButtonBuy()
 
       account = accounts[0];
     })
-    App.handle.getAddress.call(prop_id)
+    App.handle.getAddress.call(property_id)
       .then(function(addr)
         {
           owner=addr;
           console.log(addr);
-          App.handle.buyProperty(owner,account,prop_id,{from:account,to:owner})
+          App.handle.buyProperty(owner,account,property_id,{from:account,to:owner})
             .then(function(bool)
               {
                 console.log(Boolean(bool) + " transaction completed");
-                App.handle.getAddress.call(prop_id)
+                App.handle.getAddress.call(property_id)
                   .then(function(addr){console.log("Property transferred to " + addr);})
 
               }
@@ -116,7 +116,9 @@ function onButtonBuy()
 function onButtonLease()
 {
   var account,owner;
-  var prop_id = document.getElementById("prop_id").value;
+  var property_id = document.getElementById("property_id").value;
+  var property_id = document.getElementById("property_id").value;
+
   web3.eth.getAccounts(function(error, accounts) 
     {
       if (error) 
@@ -126,15 +128,80 @@ function onButtonLease()
 
       account = accounts[0];  //renter
     })
-  App.handle.getAddress.call(prop_id)
+  App.handle.getAddress.call(property_id)
       .then(function(addr)
       {
         owner=addr;
-        App.handle.leaseProperty()
+        //App.handle.leaseProperty()
       })
 
 }
 
+function setupBuy(){
+  var create_textfields = document.getElementById("text_fields");
+  create_textfields.innerHTML = ""
+  document.getElementById("time").innerHTML=""
+  document.getElementById("button_container").innerHTML=""
+  var text_field = document.createElement("input");
+  text_field.id="property_id"
+  text_field.type="text"
+  text_field.placeholder="Property ID"
+  console.log(create_textfields)
+  console.log(text_field)
+  create_textfields.appendChild(text_field);
+
+  var button = document.createElement("button")
+  button.className="button success medium-12"
+  button.innerHTML="Buy Property"
+  button.setAttribute("onclick","onButtonBuy()");
+  document.getElementById("button_container").appendChild(button);
+}
+
+function setupRegister(){
+  var create_textfields = document.getElementById("text_fields");
+  create_textfields.innerHTML=""
+  document.getElementById("time").innerHTML=""
+  document.getElementById("button_container").innerHTML=""
+  var text_field = document.createElement("input");
+  text_field.id="owner_address"
+  text_field.type="text"
+  text_field.placeholder="Owner Address"
+  create_textfields.appendChild(text_field)
+
+  var text_field1 = document.createElement("input");
+  text_field1.id="property_id"
+  text_field1.type="text"
+  text_field1.placeholder="Property ID"
+  create_textfields.appendChild(text_field1);
+
+  var button = document.createElement("button")
+  button.className="button success medium-12"
+  button.innerHTML="Register Property"
+  button.setAttribute("onclick","onButtonAdd()");
+  document.getElementById("button_container").appendChild(button);
+}
+
+function setupLease(){
+  var create_textfields = document.getElementById("text_fields");
+  create_textfields.innerHTML=""
+  document.getElementById("time").innerHTML=""
+  document.getElementById("button_container").innerHTML=""
+  var text_field = document.createElement("input");
+  text_field.id="property_id"
+  text_field.type="text"
+  text_field.placeholder="Property ID"
+  create_textfields.appendChild(text_field);
+
+  document.getElementById("time").innerHTML+='<div class="medium-4 column"><input type=text id=years placeholder="No of years"></div><div class="medium-4 column"><input type=text id=weeks placeholder="No of weeks"></div><div class="medium-4 column"><input type=text id=days placeholder="No of days"></div>'
+
+  var button = document.createElement("button")
+  button.className="button success medium-12"
+  button.innerHTML="Lease Property"
+  button.setAttribute("onclick","onButtonLease()");
+  //console.log(button)
+  document.getElementById("button_container").appendChild(button);
+  //console.log(document.getElementById("button_container"))
+}
 
 $(function() {
   $(window).load(function() {
